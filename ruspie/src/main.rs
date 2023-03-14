3338ee10b6e3dec8ca9ce3cde44cdf254a0b7547
elliot14A <akshithkatkuri14@gmail.com>
2023-03-14 11:36:05 +0530

    .

--- ruspie/src/main.rs
+++ ruspie/src/main.rs
@@ -7,8 +7,8 @@ mod startup;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     dotenvy::dotenv().ok();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let application = Application::build().await?;
     application.run_until_stopped().await?;
     Ok(())
