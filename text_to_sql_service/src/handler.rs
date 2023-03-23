f962cdaaddc5ffbb7c95cff91943ff851b014395
elliot14A <akshithkatkuri14@gmail.com>
2023-03-23 14:44:48 +0530

    refactored the code

--- text_to_sql_service/src/handler.rs
+++ text_to_sql_service/src/handler.rs
@@ -3,14 +3,18 @@ use worker::*;
 
 use crate::context::OpenAIContext;
 pub async fn handler(mut req: Request, ctx: RouteContext<OpenAIContext>) -> Result<Response> {
-    let payload = match req.json::<HandlerRequest>().await {
-        Ok(payload) => payload,
-        Err(e) => return Response::error(e.to_string(), 400),
-    };
-    match ctx.data.generate(payload.prompt.into()).await {
-        Ok(res) => Response::ok(&res.choices[0].text),
-        Err(e) => e,
-    }
+    let payload = req
+        .json::<HandlerRequest>()
+        .await
+        .map_err(|e| Response::error(e.to_string(), 400))
+        .unwrap();
+    let data = ctx
+        .data
+        .generate(payload.prompt.into())
+        .await
+        .map(|data| data.choices[0].text.to_string())
+        .unwrap();
+    Response::ok(data)
 }
 
 #[derive(Debug, Deserialize)]
