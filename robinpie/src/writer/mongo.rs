3fd1f80d311d5f2a9f44edb3245ee9c5cda39c75
elliot14A <akshithkatkuri14@gmail.com>
2023-04-26 11:56:32 +0530

    fix: resolved schema fetch issue

--- robinpie/src/writer/mongo.rs
+++ robinpie/src/writer/mongo.rs
@@ -7,7 +7,7 @@ use object_store::aws::AmazonS3;
 
 type Collection = mongodb::Collection<TableItem>;
 
-use super::{fetcher::SchemaFetcher, SchemaResponse, TableItem, Writer};
+use super::{fetcher::SchemaFetcher, TableItem, Writer};
 
 /// Writer to which fetched schemas will be stored
 /// Mongo stores schemas in a MongoDB database: String arguments are the database and collection names
@@ -31,40 +31,6 @@ impl<H: object_store::ObjectStore> MongoWriter<H> {
 #[async_trait::async_trait]
 impl Writer for MongoWriter<AmazonS3> {
     #[inline]
-    async fn fetch_from_ruspie(
-        &self,
-        filename: &str,
-        extension: &str,
-    ) -> anyhow::Result<TableItem> {
-        // set headers
-        let mut headers = reqwest::header::HeaderMap::new();
-        headers.insert("Content-Type", "application/json".parse().unwrap());
-        headers.insert("Accept", "application/json".parse().unwrap());
-        headers.insert("FILE-EXTENSION", extension.parse().unwrap());
-
-        // create new reqwest client
-        let client = reqwest::Client::new();
-        let url = std::env::var("RUSPIE_URL").unwrap_or_else(|_| "http://0.0.0.0:8080".to_string());
-        let url = format!("{}/api/schema/{}", url, filename);
-
-        // send request
-        let response = client.get(&url).headers(headers).send().await?;
-
-        // parse response to TableItem
-        let table_item = response
-            .json()
-            .await
-            .and_then(|schema_resp: SchemaResponse| {
-                Ok(TableItem {
-                    name: filename.to_string(),
-                    extension: extension.to_string(),
-                    schema: schema_resp.into(),
-                })
-            })?;
-        Ok(table_item)
-    }
-
-    #[inline]
     async fn write(&self) -> anyhow::Result<()> {
         // get list of files from S3
         let list_of_files = self.schema_fetcher().list_files().await?;
@@ -80,8 +46,14 @@ impl Writer for MongoWriter<AmazonS3> {
                 list.push(e)
             }
             if list.len() == 0 {
-                let table_item = self.fetch_from_ruspie(&file.name, &file.extension).await?;
-                self.collection.insert_one(table_item, None).await?;
+                let table_item = self
+                    .fetcher
+                    .fetch_from_ruspie(&file.name, &file.extension)
+                    .await?;
+                if let Some(table_item) = table_item {
+                    self.collection.insert_one(table_item, None).await?;
+                }
+                // self.collection.insert_one(table_item, None).await?;
             }
         }
         Ok(())
