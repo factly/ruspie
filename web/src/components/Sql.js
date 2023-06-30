35e754061b82be938dd8ff736fd08c6311d6dabb
elliot14A <akshithkatkuri14@gmail.com>
2023-06-30 15:59:13 +0530

    deleted old web dir

--- web/src/components/Sql.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useAppDataContext } from "./AppDataContext";
-
-function Sql() {
-  let { currentFileSchema, currentFileName } = useAppDataContext();
-  return <>this is sql tab</>;
-}
-
-export default Sql;
--- web/src/constants/apiEndpoints.js
+++
@@ -1,6 +0,0 @@
-export const schemaURL = process.env.REACT_APP_RUSPIE_API_URL + "/api/schema";
-export const restEndpoint =
-  process.env.REACT_APP_RUSPIE_API_URL + "/api/tables";
-export const graphqlEndpoint =
-  process.env.REACT_APP_RUSPIE_API_URL + "/api/graphql";
-export const sqlEndpoint = process.env.REACT_APP_RUSPIE_API_URL + "/api/sql";
