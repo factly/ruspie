1d32cca72ed2e7ada309ef0ec7e7aa36b65a6b5a
elliot14A <akshithkatkuri14@gmail.com>
2023-09-06 14:29:50 +0530

    feat: changes to routing

--- web/components/ui/Projects.tsx
+++ web/components/ui/Projects.tsx
@@ -1,24 +1,23 @@
 "use client";
-import React, { FC } from 'react'
-import { SearchBar } from '@/components/ui/SearchBar'
-import { Button } from './Button';
-import Icons from '../icons';
-import { Organisation } from '@/types/organisation';
+import React, { FC } from "react";
+import { Button } from "./Button";
+import Icons from "../icons";
+import { Organisation } from "@/types/organisation";
+import { SearchBar } from "./searchBar";
 
 interface OrganisationProps {
 	org: Organisation | null;
 }
 
 const Project: FC<OrganisationProps> = ({ org }) => {
-
 	const handleFilterProject = (query: string) => {
 		//
-		console.log(query)
-	}
+		console.log(query);
+	};
 
 	const handleProjectClick = () => {
 		console.log("project clicked");
-	}
+	};
 
 	const handleEditClick = () => {
 		console.log("edit clicked");
@@ -31,49 +30,55 @@ const Project: FC<OrganisationProps> = ({ org }) => {
 	return (
 		<>
 			<SearchBar
-				placeholder='Search Organisation'
+				placeholder="Search Organisation"
 				callback={handleFilterProject}
 			/>
-			<div className='flex flex-col items-center'>
-				{
-					org?.projects?.map((project, index) => (
-						<div
-							className={`flex flex-row justify-between items-center bg-white w-full p-4 border-b border-[#EAECF0]
-								${index === 0 ? 'rounded-t-md' : ''}
-								${org !== null && org.projects !== undefined && index === org.projects.length - 1 ? 'rounded-b-md' : ''}
+			<div className="flex flex-col items-center">
+				{org?.projects?.map((project, index) => (
+					<div
+						className={`flex flex-row justify-between items-center bg-white w-full p-4 border-b border-[#EAECF0]
+								${index === 0 ? "rounded-t-md" : ""}
+								${org !== null &&
+								org.projects !== undefined &&
+								index === org.projects.length - 1
+								? "rounded-b-md"
+								: ""
+							}
 							`}
-							key={org.id + "_" + project.id + "_" + project.title}
-						>
-							<div className="flex flex-col gap-2">
-								<h3 className="text-md">
-									{project.title}
-								</h3>
-								<p className="text-sm text-[#6B7280]">
-									Created at: {" "}
-									<span className="text-[#6B7280] font-semibold">
-										{project.createdAt}
-									</span>
-								</p>
-							</div>
-							<div className="flex gap-2">
-								<Button variant="outline" size="icon" className="rounded border border-[#E6E6E6]"
-									onClick={handleEditClick}
-								>
-									<Icons.EditIcon />
-								</Button>
-								<Button variant="outline" size="icon" className="rounded border border-[#E6E6E6]"
-									onClick={handleDeleteClick}
-								>
-									<Icons.TrashIcon />
-								</Button>
-							</div>
+						key={org.id + "_" + project.id + "_" + project.title}
+					>
+						<div className="flex flex-col gap-2">
+							<h3 className="text-md">{project.title}</h3>
+							<p className="text-sm text-[#6B7280]">
+								Created at:{" "}
+								<span className="text-[#6B7280] font-semibold">
+									{project.createdAt}
+								</span>
+							</p>
 						</div>
-					))
-				}
+						<div className="flex gap-2">
+							<Button
+								variant="outline"
+								size="icon"
+								className="rounded border border-[#E6E6E6]"
+								onClick={handleEditClick}
+							>
+								<Icons.EditIcon />
+							</Button>
+							<Button
+								variant="outline"
+								size="icon"
+								className="rounded border border-[#E6E6E6]"
+								onClick={handleDeleteClick}
+							>
+								<Icons.TrashIcon />
+							</Button>
+						</div>
+					</div>
+				))}
 			</div>
 		</>
+	);
+};
 
-	)
-}
-
-export default Project
+export default Project;
--- web/middleware.ts
+++ web/middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse, type NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
-  return NextResponse.redirect(new URL("/home", req.url));
+  return NextResponse.redirect(new URL("/home/organisations", req.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/"],
+  matcher: ["/", "/home"],
 };
