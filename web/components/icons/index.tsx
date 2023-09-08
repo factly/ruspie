2cc3c58652e93e9096f1f201435d2037f1b28481
elliot14A <akshithkatkuri14@gmail.com>
2023-09-08 15:07:33 +0530

    fixed svg attributes warnings

--- web/components/icons/index.tsx
+++ web/components/icons/index.tsx
@@ -1,23 +1,20 @@
-import { EditIcon } from './edit';
-import { TrashIcon } from './trash';
-import { SearchIcon } from './search';
-import { PlusIcon } from './plus'
-import { ChevronRightIcon } from './chevronRight'
-import { ArrowRight } from './arrowRight'
-import { UploadClould } from './uploadCloud';
-import { NotFound } from './NotFound';
+import { EditIcon } from "./edit";
+import { TrashIcon } from "./trash";
+import { SearchIcon } from "./search";
+import { PlusIcon } from "./plus";
+import { ChevronRightIcon } from "./chevronRight";
+import { ArrowRight } from "./arrowRight";
+import { UploadClould } from "./uploadCloud";
+import { NotFound } from "./notFound";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	EditIcon,
-	SearchIcon,
-	TrashIcon,
-	PlusIcon,
-	ChevronRightIcon,
-	ArrowRight,
-	UploadClould,
-	NotFound
-}
-
-
-
+  EditIcon,
+  SearchIcon,
+  TrashIcon,
+  PlusIcon,
+  ChevronRightIcon,
+  ArrowRight,
+  UploadClould,
+  NotFound,
+};
