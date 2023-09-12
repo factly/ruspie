d6107ca1019cdd98426cc63ae5c34e6498535206
elliot14A <akshithkatkuri14@gmail.com>
2023-09-12 15:35:45 +0530

    feat: rename created_at/updated_at to createAt/updatedAt

--- server/internal/domain/models/base.go
+++ server/internal/domain/models/base.go
@@ -9,8 +9,8 @@ import (
 // Base is the base model for all models it has all the common fields
 type Base struct {
 	ID          uint            `gorm:"primaryKey" json:"id"`
-	CreatedAt   time.Time       `json:"created_at"`
-	UpdatedAt   time.Time       `json:"updated_at"`
+	CreatedAt   time.Time       `json:"createdAt"`
+	UpdatedAt   time.Time       `json:"updatedAt"`
 	DeletedAt   *gorm.DeletedAt `gorm:"index" json:"deleted_at" swaggertype:"primitive,string"`
 	CreatedByID uint            `gorm:"column:created_by_id" json:"created_by_id"`
 	UpdatedByID uint            `gorm:"column:updated_by_id" json:"updated_by_id"`
